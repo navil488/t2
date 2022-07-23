093f60b7f28e8a67a516a2a56ca84ef386b13ca5
navil488 <109832610+navil488@users.noreply.github.com>
2022-07-22 18:17:46 -0600

    Add files via upload

---
+++ Holamundo/src/primero/principal.java
@@ -0,0 +1,16 @@
+package primero;
+
+import java.util.Scanner;
+public class principal {
+    public static void main(String[] args) {
+
+            System.out.println("hello ");
+        Scanner lectura = new Scanner (System.in);
+
+        System.out.println("Ingrese su nombre: ");
+        String nombre = lectura.next();
+
+
+        System.out.println("Bienvenido al progra 2 2022  : " + nombre );
+        }
+    }
